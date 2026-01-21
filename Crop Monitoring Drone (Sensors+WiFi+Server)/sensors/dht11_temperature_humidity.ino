9208c1cd5eedb8e7f2f0f986f6f6c2d757a57480
wehfiuefui <147403073+wehfiuefui@users.noreply.github.com>
2026-01-21 18:51:30 +0500

    Update and rename Crop Monitoring Drone (Sensors+WiFi+Server)/dht11_temperature_humidity.ino to Crop Monitoring Drone (Sensors+WiFi+Server)/sensors/dht11_temperature_humidity.ino

---
+++ Crop Monitoring Drone (Sensors+WiFi+Server)/sensors/dht11_temperature_humidity.ino
@@ -0,0 +1,31 @@
+#include <SimpleDHT.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+// for DHT11, 
+//      VCC: 5V or 3V
+//      GND: GND
+//      DATA: 2
+int pinDHT11 = 2;
+SimpleDHT11 dht11(pinDHT11);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+ Serial.begin(9600);
+}
+
+void loop() {
+ // start working...
+ Serial.println("=================================");
+ Serial.println("Sample DHT11...");
+ 
+ // read without samples.
+ byte temperature = 0;
+ byte humidity = 0;
+ int err = SimpleDHTErrSuccess;
+ if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+   Serial.print("Read DHT11 failed, err="); Serial.println(err);delay(1000);
+   return;
+ }
+}
+
